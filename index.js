20a55dc871d3e8209558aa56265a4ffdda79d2be
Dexter Buxton <dexter.buxton@gmail.com>
2018-11-26 16:25:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 function takeANumber(lineOfPeople, newCustomer) {
   // Add the customer to the line of people
   lineOfPeople.push(newCustomer);
+  
   // Determine the number for this customer
   let currentNumber = lineOfPeople.length;
-  return `Welcome, ${newCustomer}. You are number ${currentNumber} in line.`
+  
+  return `Welcome, ${newCustomer}. You are number ${currentNumber} in line.`;
 }
 
 function nowServing(lineOfPeople){
-  // Remove the person at the beginning of the lineOfPeople
+  // Deter
   if lineOfPeople.length === 0 {
-    return 
+    return "There is nobody waiting to be served!";
   }
+  
+  // Remove the person at the beginning of the lineOfPeople
   let currentCustomer = lineOfPeople.shift()
   
 }
