317a9d1ea4c2d597c73e5f1f798780a2055c060e
Dexter Buxton <dexter.buxton@gmail.com>
2018-11-26 16:20:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function takeANumber(array, name) {
   // Add the customer to the array
   array.push(name);
   // Determine the number for this customer
-  let currentNumber = array.length + 1;
+  let currentNumber = array.length;
   return `Welcome, ${name}. You are number ${currentNumber} in line.`
 }
 
